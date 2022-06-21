3276fd05ff357f5202504060355f87a284931e97
From3 <56982761+From3@users.noreply.github.com>
2022-06-22 01:52:16 +0300

    Add encoding

--- encryption_tools.py
+++ encryption_tools.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*- #
+
 import base64
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
